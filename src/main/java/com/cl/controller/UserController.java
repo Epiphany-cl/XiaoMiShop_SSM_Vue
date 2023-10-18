7e310d1d74adceec94bf911ac8ba4c084dc0523b
Epiphany-cl <979877240@qq.com>
2023-10-19 00:29:28 +0800

    register.html to be continued

--- src/main/java/com/cl/controller/UserController.java
+++ src/main/java/com/cl/controller/UserController.java
@@ -3,8 +3,10 @@ package com.cl.controller;
 
 import com.cl.pojo.User;
 import com.cl.vo.ResultVO;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
@@ -19,12 +21,16 @@ public class UserController {
 
     //http://localhost:8080/XiaoMiShop_SSM_Vue/user/register
     @RequestMapping("/register")
-    public ResultVO<Object> register(HttpSession session, @RequestBody User user){
-        System.out.println("user = " + user);
+    public ResultVO<Object> register(HttpSession session,
+                                     User user,
+                                     String verificationCode) {
 
         String token = (String) session.getAttribute(KAPTCHA_SESSION_KEY);
         session.removeAttribute(KAPTCHA_SESSION_KEY);
+        //Todo 注册逻辑
+        System.out.println("user = " + user);
+        System.out.println("verificationCode = " + verificationCode);
         System.out.println("token = " + token);
-        return null;
+        return new ResultVO<>();
     }
 }
